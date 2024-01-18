8af13dc44053b707b5f1bed9bb9b2e2e49874a2d
CesarJavier09 <cesarfoc22@gmail.com>
2024-01-18 00:01:52 -0500

    changing migration

--- db/migrations/20240116165841-create-customer.js
+++ db/migrations/20240116165841-create-customer.js
@@ -1,15 +1,18 @@
 'use strict';
 
 const {customerSchema, CUSTOMER_TABLE} = require('./../models/customer.model');
+const {ORDER_TABLE,orderSchema} = require('./../models/order.model');
 
 /** @type {import('sequelize-cli').Migration} */
 
 module.exports = {
   async up (queryInterface) {
+    queryInterface.createTable(ORDER_TABLE,orderSchema);
     await queryInterface.createTable(CUSTOMER_TABLE,customerSchema);
   },
 
   async down (queryInterface) {
+    queryInterface.dropTable(ORDER_TABLE);
    await queryInterface.dropTable(CUSTOMER_TABLE);
   }
 };
