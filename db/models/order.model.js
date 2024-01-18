5f3012dfd5140a3ee11b724a64288f999aef75ba
CesarJavier09 <cesarfoc22@gmail.com>
2024-01-18 00:17:14 -0500

    virtual removed

--- db/models/order.model.js
+++ db/models/order.model.js
@@ -33,17 +33,6 @@ const orderSchema = {
     onUpdate: 'CASCADE',
     onDelete: 'SET NULL',
   },
-  total: {
-    type: DataTypes.VIRTUAL,
-    get(){
-      if(this.items.length > 0){
-        return this.items.reduce((total, item) => {
-          return total + (item.price * item.OrderProduct.amount);
-        }, 0);
-      }
-      return 0;
-    }
-  }
 }
 
 class Order extends Model {
